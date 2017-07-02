10621d8f96d472ecff4fa928d098372344ed3658
ziyu <494999197@qq.com>
2017-07-02 17:38:58 +0800

    git-add-content

---
+++ app/reducer/rootReducer.js
@@ -0,0 +1,12 @@
+/**
+ * 这个方法是用来整合所有的 combineReducers => Reducer
+ * @type {[type]}
+ */
+
+import { combineReducers } from 'redux';
+
+import Home from './homeReducer.js';
+
+export default rootReducer = combineReducers({
+  Home
+})
---
+++ app/store/store.js
@@ -0,0 +1,14 @@
+/**
+ * 创建 store , 整合Provider
+ * create by 2017-06
+ */
+import thunk from 'redux-thunk';
+import { createStore, applyMiddleware, compose } from 'redux';
+import rootReducer from "./../reducer/rootReducer.js";
+
+
+let store = createStore(Reducer, {}, compose(
+  applyMiddleware(thunk),
+  window.devToolsExtension ? window.devToolsExtension() : f => f
+))
+export default store;
